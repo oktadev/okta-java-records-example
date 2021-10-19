9d20494263fe606f0924bd2c423b177e9e287be7
indiepopart <jimena@gmail.com>
2021-10-19 00:15:23 -0300

    remove blanks

--- src/main/java/com/okta/developer/records/repository/LocalDateConverter.java
+++ src/main/java/com/okta/developer/records/repository/LocalDateConverter.java
@@ -4,8 +4,8 @@ import org.springframework.core.convert.converter.Converter;
 
 import java.time.LocalDate;
 
-
 public class LocalDateConverter implements Converter<String, LocalDate> {
+
     @Override
     public LocalDate convert(String s) {
         return LocalDate.parse(s);
