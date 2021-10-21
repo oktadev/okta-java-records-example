5fa133deddb18bdfda2fcc080f9b216169bd50a9
indiepopart <jimena@gmail.com>
2021-10-21 13:47:23 -0300

    update id type in repository

--- src/main/java/com/okta/developer/records/repository/StatsRepository.java
+++ src/main/java/com/okta/developer/records/repository/StatsRepository.java
@@ -3,6 +3,6 @@ package com.okta.developer.records.repository;
 import com.okta.developer.records.domain.EndOfGame;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
-public interface StatsRepository extends ReactiveSortingRepository<EndOfGame, Long>, MentalStateStatsRepository {
+public interface StatsRepository extends ReactiveSortingRepository<EndOfGame, String>, MentalStateStatsRepository {
 
 }
